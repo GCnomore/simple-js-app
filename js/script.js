48d17e3e395f0337ee08f061d4879a3964bfcea0
GCnomore <isaacgc08@gmail.com>
2020-08-17 13:43:57 -0700

    Added some pokemon profiles to an array

--- js/script.js
+++ js/script.js
@@ -1,5 +1,37 @@
-alert('Hello world');
-
-var favoriteFood = 'Hello World';
-
-document.write(favoriteFood);
+var pokemonList = [
+  {
+    name: 'Pikachu',
+    height: 0.4,
+    type: 'Electric',
+  },
+  {
+    name: 'Charmeleon',
+    height: 1.1,
+    type: 'Fire',
+  },
+  {
+    name: 'Alakazam',
+    height: 1.5,
+    type: 'Phychic',
+  },
+  {
+    name: 'Dugtrio',
+    height: 0.7,
+    type: 'Ground',
+  },
+  {
+    name: 'Gyarados',
+    height: 6.5,
+    type: ['Water', 'Flying'],
+  },
+  {
+    name: 'Zapdos',
+    height: 1.6,
+    type: ['Electric', 'Flying'],
+  },
+  {
+    name: 'Hitmonlee',
+    height: 1.5,
+    type: 'Fighting',
+  },
+];
