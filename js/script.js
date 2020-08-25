7435a2eda74bf34eb0471293019b8c01c7812ffc
GCnomore <isaacgc08@gmail.com>
2020-08-25 04:15:19 -0700

    Made modal window to pop up when clicked Poke button

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var pokemonRepository = (function () {
   var pokemonList = [];
-
+  var modalContainer = document.querySelector('#modal-container');
+  modalContainer.classList.add('hidden');
   var apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
 
   function showLodaingMessage() {
@@ -63,18 +64,67 @@ var pokemonRepository = (function () {
 
   function add(pokemon) {
     pokemonList.push(pokemon);
-    console.log(pokemonList);
-    // if (
-    //   typeof pokemon === 'object' &&
-    //   'name' in pokemon &&
-    //   'height' in pokemon &&
-    //   'type' in pokemon
-    // ) {
-    // }
   }
   function showDetails(pokemon) {
+    //Adding HTML elements
+    var modal = document.createElement('div');
+    var buttonContainer = document.createElement('div');
+    var closeButton = document.createElement('button');
+    var pokeName = document.createElement('h1');
+    var pokeHeight = document.createElement('span');
+    var imgContainer = document.createElement('div');
+    var pokeImg = document.createElement('img');
+    var pokeType = document.createElement('ul');
+    modal.classList.add('modal');
+    pokeImg.classList.add('pokeImg');
+    buttonContainer.classList.add('buttonContainer');
+    imgContainer.classList.add('imgContainer');
+    pokeType.classList.add('pokeType');
+
     loadDetails(pokemon).then(() => {
-      console.log(pokemon);
+      //Adding contents to HTML elements created above
+      closeButton.innerText = 'X';
+      pokeName.innerText = pokemon.name;
+      pokeHeight.innerText = 'Height: ' + pokemon.height + ' M';
+      buttonContainer.appendChild(closeButton);
+      modal.appendChild(buttonContainer);
+      modal.appendChild(pokeName);
+      modal.appendChild(pokeHeight);
+      pokemon.types.forEach((item) => {
+        var typeList = document.createElement('li');
+        typeList.innerText = item.type.name;
+        modal.appendChild(pokeType);
+        pokeType.appendChild(typeList);
+        typeList.classList.add(typeList.textContent);
+      });
+      imgContainer.appendChild(pokeImg);
+      modal.appendChild(imgContainer);
+      pokeImg.setAttribute('src', pokemon.imageUrl);
+      modalContainer.appendChild(modal);
+
+      function showModal() {
+        modalContainer.classList.remove('hidden');
+        document.querySelector('.pokedex').classList.add('modalBackground');
+
+        if (modalContainer.classList != 'hidden') {
+          closeButton.addEventListener('click', hideModal);
+          window.addEventListener('keydown', hideModal);
+          document
+            .querySelector('.pokedex')
+            .addEventListener('click', hideModal);
+        }
+
+        function hideModal() {
+          modalContainer.classList.add('hidden');
+          while (modalContainer.firstChild) {
+            modalContainer.removeChild(modalContainer.firstChild);
+          }
+          document
+            .querySelector('.pokedex')
+            .classList.remove('modalBackground');
+        }
+      }
+      showModal();
     });
   }
 
